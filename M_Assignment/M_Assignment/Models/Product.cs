5d7d575bf1af1a992fb5752dd6d1aa99080bf96f
varun <varun1malik@gmail.com>
2020-09-08 21:17:51 +0530

    Added models for the project

---
+++ M_Assignment/M_Assignment/Models/Product.cs
@@ -0,0 +1,6 @@
+namespace M_Assignment.Models{
+    public class Product {
+        public ProductType Sku {get; set;}
+        public int Quantity {get; set;}
+    }
+}
---
+++ M_Assignment/M_Assignment/Models/ProductType.cs
@@ -0,0 +1,9 @@
+namespace M_Assignment.Models
+{
+    public enum ProductType{
+        A=50,
+        B=30,
+        C=20,
+        D=15
+    }
+}
